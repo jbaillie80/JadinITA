3e894593c46ab22a6e0486d6bb0ab8fd807389ae
jbaillie80 <jbaillie@wisc.edu>
2021-12-27 15:59:33 -0600

    summary

---
+++ Jadin/VariablesOutline.py
@@ -0,0 +1,58 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list):
+
+a=1
+b=2.2
+c="Words"
+d=True
+e=["Risk", "of", "Rain"]
+
+
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+
+a=22
+A=33
+
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+
+f=99
+f=100
+
+#4)
+#Make a variable and set it to a variable you have already created:
+
+d=False
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+
+e=["Risk ", "of ", "Rain"]
+g=e[1]
+
+#6)
+#Make a String set to your name. Make a variable that is set to the first
+#character in that string:
+
+name="Jadin"
+letter=name[0]
+
+#7)
+#Print out ONE of any of the variables you have created so far:
+
+print(e)
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+
+h=false
