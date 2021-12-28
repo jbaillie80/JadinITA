16ce02e4c77ad6a8fe1eecb89500615bcc234069
jbaillie80 <jbaillie@wisc.edu>
2021-12-28 09:24:02 -0600

    s

---
+++ Jadin/CommentsOutline.py
@@ -0,0 +1,22 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+
+#Yup
+
+#2) Make a multi-line comment.
+
+'''
+Yup
+Yup
+'''
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+x=1 #Yup
+
+
