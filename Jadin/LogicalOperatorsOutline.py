716c11b2f23649c13cca5722e39d5a0f040a9aa0
jbaillie80 <jbaillie@wisc.edu>
2021-12-28 11:28:48 -0600

    s

---
+++ Jadin/LogicalOperatorsOutline.py
@@ -0,0 +1,29 @@
+'''
+This outline will help solidify concepts from the Logical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+#EX) Make two boolean variables. Put them on either side of the and operator.
+#Store this expression in a variable named a. Print the variable.
+from pickle import TRUE
+one = True
+two = False
+a = one and two 
+print(a)
+#1) Make two boolean variables. Put them on either side of the and operator.
+#Store this expression in a variable named a. Print the variable.
+t=True
+f=False
+a=t and f
+print(a)
+#2) Make two boolean variables. Put them on either side of the or operator.
+#Store this expression in a variable named b. Print the variable.
+t=True
+f=False
+b=t or f
+print(b)
+#3) Make one boolean variable. Put the variable after the not. Store this 
+#expression in a variable named c. Print the variable.
+t=True
+c=not t
+print(c)
+#4) Make a logical expression with one of the common SYNTAX errors.
