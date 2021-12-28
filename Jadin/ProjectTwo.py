b551c6bd8e9c2ba2694b8e3756bd084a2caf8d36
jbaillie80 <jbaillie@wisc.edu>
2021-12-28 11:52:13 -0600

    s

---
+++ Jadin/ProjectTwo.py
@@ -0,0 +1,6 @@
+miles=input("How many miles did you travel?")
+miles=int(miles)
+yards=miles*1760
+feet=miles*5280
+inches=miles*63360
+print("You traveled " + str(miles) + " miles, " + str(yards) + " yards, " + str(feet) + " feet, " + str(inches) + " inches.")
