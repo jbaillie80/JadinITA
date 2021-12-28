7001f4041abe9133d43f628c0d41a7dec7c285a4
jbaillie80 <jbaillie@wisc.edu>
2021-12-28 09:47:31 -0600

    s

---
+++ Jadin/ProjectOne.py
@@ -0,0 +1,15 @@
+'''
+Created on Dec 28, 2021
+
+@author: jadin
+'''
+name=input("What is your name?")
+color=input("What is your favorite color?")
+city=input("What city did you grow up in?")
+friend=input("What is your best friend’s name?")
+hair=input("What color hair do you have?")
+print(name)
+print(color)
+print(city)
+print(friend)
+print(hair)
