ddeb6bcdb2668b7829e1cc31e23f4cc2bef9734d
jbaillie80 <jbaillie@wisc.edu>
2022-01-05 10:38:47 -0600

    Summary

---
+++ Teaching/ComparisonOperators/Sample.py
@@ -0,0 +1,33 @@
+#First+Comparison+Second
+
+4 == 4
+#True
+
+4 == 3
+#False
+
+False == False
+#True
+
+4 > 3
+#True
+
+4<3
+#False
+
+4 >= 4
+#True
+
+3 >= 4
+#False
+
+5>= 1
+#True
+
+v = (5 == 5)
+print(v)
+
+g = (4 > 5)
+print(g)
+
+print(v == g)
